fb0fc2772e0f32a22fedb7019245c8a795cb92dd
Exsper <starlightexsper@foxmail.com>
2026-08-09 10:01:58 +0800

    更新 StableCompatLib 构建配置，启用静态链接并添加错误检查以确保库存在

--- StableCompatLib/build.bat
+++ StableCompatLib/build.bat
@@ -1,2 +1,2 @@
 g++ -c StableCompatLib.cpp
-g++ -shared -lm -o StableCompatLib.dll StableCompatLib.o
+g++ -shared -static -static-libgcc -static-libstdc++ -lm -o StableCompatLib.dll StableCompatLib.o
